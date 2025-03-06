701883ebffa991a325328a2293fd2ebfe4fb754b
Patricia Adamsson <patricia.adamsson@capgemini.com>
2025-03-06 20:36:07 +0100

    Update prettier and format files

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -33,7 +33,10 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
       '@typescript-eslint/no-explicit-any': 'warn',
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
@@ -45,15 +48,15 @@ export default tseslint.config(
       'no-debugger': 'warn',
       'no-unused-vars': 'warn',
       'no-shadow': 'warn',
-      'eqeqeq': ['error', 'always'],
+      eqeqeq: ['error', 'always'],
       'no-duplicate-imports': 'error',
       'no-redeclare': 'error',
       'no-duplicate-case': 'error',
       'no-else-return': 'error',
       'prefer-const': 'error',
       'consistent-this': ['warn', 'self'],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
       'no-empty-function': 'error',
       'no-magic-numbers': 'warn',
       'no-unneeded-ternary': 'error',
@@ -65,5 +68,5 @@ export default tseslint.config(
       'jsx-a11y/no-redundant-roles': 'warn',
       'jsx-a11y/heading-hierarchy': 'warn',
     },
-  },
-)
+  }
+);
