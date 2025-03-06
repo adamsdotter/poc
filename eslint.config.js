9d932858031bd0699e6c3bf29c52f6193ee593ac
Patricia Adamsson <patricia.adamsson@capgemini.com>
2025-03-06 10:31:31 +0100

    Add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,12 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'plugin:prettier/recommended',
+      'prettier',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +21,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: 'eslint-plugin-prettier',
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +29,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',
     },
   },
 )
