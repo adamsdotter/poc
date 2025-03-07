cbef548225a0ed1ffe992f9ca9d7ee07c5e3f5e4
Patricia Adamsson <patricia.adamsson@capgemini.com>
2025-03-07 12:29:09 +0100

    Add SASS support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Global SCSS
+        additionalData: `@use '/src/styles/global.scss' as *;`,
+      },
+    },
+  },
 });
